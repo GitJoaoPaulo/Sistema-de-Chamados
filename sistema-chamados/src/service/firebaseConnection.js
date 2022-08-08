f36325ff97cbd232a13692edae58404d6c7066d9
GitJoaoPaulo <joaopaulobt@outlook.com>
2022-08-08 17:13:15 -0300

    Criação do projeto e configuração do firebase

---
+++ sistema-chamados/src/service/firebaseConnection.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyDfHKF6dqdb3cefosA-yfJv9map2MwLQx4",
+    authDomain: "sistema-chamados-166fb.firebaseapp.com",
+    projectId: "sistema-chamados-166fb",
+    storageBucket: "sistema-chamados-166fb.appspot.com",
+    messagingSenderId: "77240884404",
+    appId: "1:77240884404:web:e2603c6eacb69a63048be1",
+    measurementId: "G-0V2MP2LL51"
+  };
+  
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
