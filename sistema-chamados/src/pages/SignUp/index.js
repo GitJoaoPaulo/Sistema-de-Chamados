9f5587a3d6eb8ce2f66ff9aa567559aca8e6f3a5
GitJoaoPaulo <joaopaulobt@outlook.com>
2022-08-12 16:15:58 -0300

    Criação da tela de cadastro

--- sistema-chamados/src/pages/SignUp/index.js
+++ sistema-chamados/src/pages/SignUp/index.js
@@ -1,10 +1,41 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.png';
+
 function SignUp() {
-    return (
-      <div>
-          <h1>Página de cadastro</h1>
-      </div>
-    );
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nome, setNome] = useState("");
+
+
+  function handleSubimit(e){
+    e.preventDefault();
+    alert('Teste')
   }
-  
-  export default SignUp;
+
+  return (
+    <div className='container-center'>
+      <div className='login'>
+        
+        <div className='logo-area'>
+          <img src={logo} alt="Sistema-Logo"/>
+        </div>
+
+        <form onSubmit={handleSubimit}>
+          <h1>Cadastrar uma conta</h1>
+          <input type="text" placeholder='Digite seu nome' value={nome} onChange={(e) => setNome(e.target.value)}/>
+          <input type="text" placeholder='email@exemplo.com' value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <input type="password" placeholder='Crie uma senha' value={password} onChange={(e) => setPassword(e.target.value)}/>
+          <button type='submit'>Criar Conta</button>
+        </form>
+
+        <Link to="/">Já tem uma conta ? Entre </Link>
+
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
   
