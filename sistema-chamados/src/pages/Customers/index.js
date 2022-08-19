97e112220f142d1e9308db71d8ec8b93e660aa13
GitJoaoPaulo <joaopaulobt@outlook.com>
2022-08-19 17:24:55 -0300

    Criação da página de Novo Cliente

---
+++ sistema-chamados/src/pages/Customers/index.js
@@ -0,0 +1,51 @@
+import './customers.css';
+import { useState } from 'react';
+
+import Header from '../../components/Header';
+import Title from '../../components/Title';
+
+import { FiUser } from 'react-icons/fi';
+
+function Customers() {
+
+    const [nomeEmpresa, setNomeEmpresa] = useState("");
+    const [cnpj, setCnpj] = useState("");
+    const [endereco, setEndereco] = useState("");
+
+
+    function handleAdd(e){
+        e.preventDfault();
+
+    }
+
+    return (
+        <div>
+            <Header />
+            <div className='content'>
+                <Title name="Clientes">
+                    <FiUser size={24} />
+                </Title>
+
+                <div className='container'>
+                    <form className='form-profile customers' onSubmit={handleAdd}>
+                        <label>Nome da Empresa</label>
+                        <input type="text" placeholder='Nome da sua empresa' value={nomeEmpresa} onChange={(e) => setNomeEmpresa(e.target.value)}/>
+
+                        <label>CNPJ</label>
+                        <input type="text" placeholder='CNPJ da sua empresa' value={cnpj} onChange={(e) => setCnpj(e.target.value)}/>
+
+                        <label>Endereço</label>
+                        <input type="text" placeholder='Endereço da sua empresa' value={endereco} onChange={(e) => setEndereco(e.target.value)}/>
+                    
+                        <button type='submit'>Cadastrar</button>
+                    
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Customers;
