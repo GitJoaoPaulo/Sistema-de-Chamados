4eb9aaabf43badc345d038fe151567e7a496269b
GitJoaoPaulo <joaopaulobt@outlook.com>
2022-08-19 18:12:48 -0300

    Cadastro de novos clientes no banco de dados

--- sistema-chamados/src/pages/Customers/index.js
+++ sistema-chamados/src/pages/Customers/index.js
@@ -1,10 +1,12 @@
-import './customers.css';
 import { useState } from 'react';
+import firebase from '../../service/firebaseConnection';
+import './customers.css';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
 
 import { FiUser } from 'react-icons/fi';
+import { toast } from 'react-toastify';
 
 function Customers() {
 
@@ -13,8 +15,29 @@ function Customers() {
     const [endereco, setEndereco] = useState("");
 
 
-    function handleAdd(e){
-        e.preventDfault();
+    async function handleAdd(e) {
+        e.preventDefault();
+
+        if (nomeEmpresa !== "" && cnpj.length >= 14 && cnpj !== "" && endereco !== ""){
+            await firebase.firestore().collection("customers").add({
+                nome: nomeEmpresa,
+                cnpj: cnpj,
+                endereco: endereco
+            })
+            .then(() => {
+                setNomeEmpresa("");
+                setCnpj("");
+                setEndereco("");
+                toast.success("Empresa cadastrada com sucesso");
+            })
+            .catch((error) => {
+                console.log(error);
+                toast.error("Erro ao cadastrar a empresa")
+            })
+        }else{
+            console.log("erro")
+            toast.error("Verifique se o CNPJ tem 14 digitos ou se os campos foram preenchidos corretamente ")
+        }
 
     }
 
@@ -29,16 +52,16 @@ function Customers() {
                 <div className='container'>
                     <form className='form-profile customers' onSubmit={handleAdd}>
                         <label>Nome da Empresa</label>
-                        <input type="text" placeholder='Nome da sua empresa' value={nomeEmpresa} onChange={(e) => setNomeEmpresa(e.target.value)}/>
+                        <input type="text" placeholder='Nome da sua empresa' value={nomeEmpresa} onChange={(e) => setNomeEmpresa(e.target.value)} />
 
                         <label>CNPJ</label>
-                        <input type="text" placeholder='CNPJ da sua empresa' value={cnpj} onChange={(e) => setCnpj(e.target.value)}/>
+                        <input type="text" placeholder='CNPJ da sua empresa' value={cnpj} onChange={(e) => setCnpj(e.target.value)} />
 
                         <label>Endereço</label>
-                        <input type="text" placeholder='Endereço da sua empresa' value={endereco} onChange={(e) => setEndereco(e.target.value)}/>
-                    
+                        <input type="text" placeholder='Rua número, vila, estado' value={endereco} onChange={(e) => setEndereco(e.target.value)} />
+
                         <button type='submit'>Cadastrar</button>
-                    
+
                     </form>
                 </div>
 
