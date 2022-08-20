f128d2d4ebbe0a4e344578046070395bc7eb28f2
GitJoaoPaulo <joaopaulobt@outlook.com>
2022-08-20 13:27:05 -0300

    Criação da Tabela de chamados e responsividade

--- sistema-chamados/src/pages/Dashboard/index.js
+++ sistema-chamados/src/pages/Dashboard/index.js
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 import Title from '../../components/Title';
 import Header from '../../components/Header';
 
-import { FiMessageCircle, FiPlus } from 'react-icons/fi';
+import { FiMessageCircle, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi';
 
 
 function Dashboard() {
 
-  const [chamados, setChamados] = useState([]);
+  const [chamados, setChamados] = useState([1]);
 
   return (
     <div>
@@ -40,6 +40,42 @@ function Dashboard() {
               <FiPlus color='#FFF' size={24} />
               Novo chamado
             </Link>
+
+            <table>
+
+              <thead>
+                <tr>
+                  <th scope='col'>Cliente</th>
+                  <th scope='col'>Assunto</th>
+                  <th scope='col'>Status</th>
+                  <th scope='col'>Cadastrado em</th>
+                  <th scope='col'>#</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td data-label="Cliente">Sujeito Programador</td>
+                  <td data-label="Assunto">Suporte</td>
+                  <td data-label="Status">
+                    <span className='badge' style={{ backgroundColor: "#5cb85c" }}>Em aberto</span>
+                  </td>
+                  <td data-label="Cadastrado">26/01/2019</td>
+                  <td data-label="#">
+                    <button className='action' style={{ backgroundColor: '#3583f6' }}>
+                      <FiSearch color='#FFF' size={17} />
+                    </button>
+
+                    <button className='action' style={{ backgroundColor: '#F6a935' }}>
+                      <FiEdit2 color='#FFF' size={17} />
+                    </button>
+                  </td>
+
+                </tr>
+              </tbody>
+
+            </table>
+
           </>
         )
 
